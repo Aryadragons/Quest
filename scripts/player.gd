144aa9d2d3587a9e19503c05d1ab3cf0ca758db9
Aryadragons <sunnybgp@icloud.com>
2024-12-23 14:56:48 -0600

    more progress
    got death working but its slow, level building progress, and changed the size of the character

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,16 +1,18 @@
 extends CharacterBody2D
 
 
-const SPEED = 175.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 
 
-func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta * 1.5
 
-	# Handle jump.
+
+func _physics_process(delta: float) -> void:
+	#Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta * 1.25
+
+	#Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
